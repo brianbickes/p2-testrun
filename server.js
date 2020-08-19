e7409e1b75e1d55a513b49c06b71176f3e90360e
brianbickes <brianbickes@gmail.com>
2020-08-19 08:29:28 -0700

    Fixed mongo issue

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(methodOverride('_method'));
-const mongoURI = process.env.mongoURI
+const mongoURI = process.env.MONGO_URI
 // mongoose connection
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
